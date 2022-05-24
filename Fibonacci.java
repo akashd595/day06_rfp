019a6fcdb5a25591761ce12b94b081736244b5f2
Akash Das <akashd595@gmail.com>
2022-05-24 13:50:36 +0530

    ADDED: fibonacci series

---
+++ Fibonacci.java
@@ -0,0 +1,23 @@
+package com.bridgelabz.day06;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter count ");
+        int num = sc.nextInt();
+        int n1 = 0;
+        int n2 = 1;
+        System.out.println(n1);
+        System.out.println(n2);
+
+        while (num > 2){
+            int sum=n1+n2;
+            System.out.println(sum);
+            n1=n2;
+            n2=sum;
+            num--;
+        }
+    }
+}
